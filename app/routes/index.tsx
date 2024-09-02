c6a028e3429f78edd0a5d85f0cf68542165a0a0e
mykhailoivankiv <mivankiv@icloud.com>
2024-09-02 14:01:44 +0200

    Update some packages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,4 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 
-export const loader: LoaderFunction = async ({ request }) => {
-  return redirect('/clients')
-}
+export const loader: LoaderFunction = async ({ request }) => redirect('/clients')
