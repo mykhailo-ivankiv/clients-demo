dd503d9f63326a45ad74c9c1ba35c36583ec5e72
mykhailoivankiv <mivankiv@icloud.com>
2024-09-02 17:46:17 +0200

    Fix remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-exports = {
+module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
   serverModuleFormat: 'cjs',
